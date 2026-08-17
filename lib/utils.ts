2103ee7e317d0d48860fd932246c0afee6fc4dfd
liquid-dre <avdingiswayo54@gmail.com>
2026-08-17 15:01:59 +0200

    Vendor bklit line + bar chart components

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// Standard shadcn/ui class combiner: clsx for conditionals, tailwind-merge to
-// dedupe conflicting utilities. Used by every shadcn-derived component.
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
